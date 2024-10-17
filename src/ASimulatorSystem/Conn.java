2b7e2e5db058ad4072dc00236daea5ff7e70e84a
Sanchit Anand <145704254+sanchitanand1@users.noreply.github.com>
2024-10-17 17:32:18 +0530

    Update Conn.java

--- src/ASimulatorSystem/Conn.java
+++ src/ASimulatorSystem/Conn.java
@@ -8,7 +8,7 @@ public class Conn{
     public Conn(){  
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");  
-            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","anand@2643");    
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","password");    
             s =c.createStatement(); 
            
           
